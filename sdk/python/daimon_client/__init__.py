0ef4041484b7a83ef3a4c7a5f7cb2589a325ec41
sonicboom15 <rishikumar15@gmail.com>
2026-05-06 13:24:57 +0530

    chore: bumped python sdk

--- sdk/python/daimon_client/__init__.py
+++ sdk/python/daimon_client/__init__.py
@@ -2,6 +2,6 @@ from ._async_client import AsyncClient
 from ._client import Client
 from ._types import Chunk, DaimonError, Message, Tool, ToolCall
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["Client", "AsyncClient", "Message", "Tool", "ToolCall", "Chunk", "DaimonError", "__version__"]
